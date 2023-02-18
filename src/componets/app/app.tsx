ad77b90302bbecaf1b7ea007a6f622c625eff173
Igor Petrov <galdenysh@MacBook-Air-Igor.local>
2023-02-18 09:51:18 +0300

    feat: add 404 page

--- src/componets/app/app.tsx
+++ src/componets/app/app.tsx
@@ -6,17 +6,22 @@ import PatchNote from '../../pages/patch-note/patch-note';
 import { links } from '../../utils/constants';
 import Analyze from '../../pages/analize/analize';
 import Game from '../../pages/game/game';
+import NotFound from '../../pages/not-found/not-found';
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path={links.main} element={<Navigate to={links.news} replace />} />
+        <Route
+          path={links.main}
+          element={<Navigate to={links.news} replace />}
+        />
         <Route path={links.news} element={<News />} />
         <Route path={links.patchNote} element={<PatchNote />} />
         <Route path={links.analyze} element={<Analyze />} />
         <Route path={links.game} element={<Game />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </>
---
+++ src/pages/not-found/not-found.tsx
@@ -0,0 +1,11 @@
+import styles from './not-found.module.css';
+
+function NotFound() {
+  return (
+    <main className={styles.content}>
+      <h2 className={styles.mainTitle}>404</h2>
+    </main>
+  );
+}
+
+export default NotFound;
