0f525de3e9553b1fe15afd6ca6c3926ebcf3cb0d
Igor Petrov <galdenysh@MacBook-Air-Igor.local>
2023-02-19 10:12:28 +0300

    feat: add patch note card

--- src/componets/cards-patches/cards-patches.tsx
+++ src/componets/cards-patches/cards-patches.tsx
@@ -1,9 +1,25 @@
+import { patches } from '../../utils/mock';
 import styles from './cards-patches.module.css';
 
 function CardsPatches() {
   return (
     <section className={styles.cardsPatches}>
       <h2 className={styles.mainTitle}>Patch Notes</h2>
+      <ul className={styles.list}>
+        {patches.map((item) => (
+          <li className={styles.listItem} key={item.id}>
+            <article className={styles.container}>
+              <img
+                className={styles.image}
+                src={item.image}
+                alt="Изображение новости"
+              ></img>
+              <h3 className={styles.title}>{item.title}</h3>
+              <p className={styles.text}>{item.text}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -1,42 +1,62 @@
 export const news = [
   {
-    id: "1",
-    link: "https://owlcat.games/news/78",
-    title: "OwlCat",
-    image: "https://i.ibb.co/6yKj2vq/pathfinder.jpg",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    explanation: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+    id: '1',
+    link: 'https://owlcat.games/news/78',
+    title: 'OwlCat',
+    image: 'https://i.ibb.co/6yKj2vq/pathfinder.jpg',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    explanation:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
   },
   {
-    id: "2",
-    link: "https://owlcat.games/news/78",
-    title: "OwlCat",
-    image: "https://i.ibb.co/6yKj2vq/pathfinder.jpg",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    explanation: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+    id: '2',
+    link: 'https://owlcat.games/news/78',
+    title: 'OwlCat',
+    image: 'https://i.ibb.co/6yKj2vq/pathfinder.jpg',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    explanation:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
   },
   {
-    id: "3",
-    link: "https://owlcat.games/news/78",
-    title: "OwlCat",
-    image: "https://i.ibb.co/6yKj2vq/pathfinder.jpg",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    explanation: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+    id: '3',
+    link: 'https://owlcat.games/news/78',
+    title: 'OwlCat',
+    image: 'https://i.ibb.co/6yKj2vq/pathfinder.jpg',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    explanation:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
   },
   {
-    id: "4",
-    link: "https://owlcat.games/news/78",
-    title: "OwlCat",
-    image: "https://i.ibb.co/6yKj2vq/pathfinder.jpg",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    explanation: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+    id: '4',
+    link: 'https://owlcat.games/news/78',
+    title: 'OwlCat',
+    image: 'https://i.ibb.co/6yKj2vq/pathfinder.jpg',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    explanation:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
   },
   {
-    id: "5",
-    link: "https://owlcat.games/news/78",
-    title: "OwlCat",
-    image: "https://i.ibb.co/6yKj2vq/pathfinder.jpg",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    explanation: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
-  }
-]
+    id: '5',
+    link: 'https://owlcat.games/news/78',
+    title: 'OwlCat',
+    image: 'https://i.ibb.co/6yKj2vq/pathfinder.jpg',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    explanation:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+  },
+];
+
+export const patches = [
+  {
+    id: '1',
+    title: 'Patch 1',
+    image: 'https://i.ibb.co/6yKj2vq/pathfinder.jpg',
+    text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+  },
+  {
+    id: '2',
+    title: 'Patch 2',
+    image: 'https://i.ibb.co/6yKj2vq/pathfinder.jpg',
+    text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+  },
+];
