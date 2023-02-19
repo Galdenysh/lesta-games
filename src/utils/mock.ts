201f4b12d2126176f7d87742306b662d7dfa241d
Igor Petrov <galdenysh@MacBook-Air-Igor.local>
2023-02-19 17:41:51 +0300

    feat: add content for news page

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -3,46 +3,49 @@ export const news = [
     id: '1',
     link: 'https://owlcat.games/news/78',
     title: 'OwlCat',
-    image: 'https://i.ibb.co/6yKj2vq/pathfinder.jpg',
-    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    image: 'https://i.ibb.co/X4B673B/Pathfinder.jpg',
+    about:
+      'Новость об успехе студии. Демонстрирует мотивацию разработчиков к развитию продукта',
     explanation:
-      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+      'Дизайн оформлен в стиле остальный новостных публикаций. Фраза  “Great news, Crusaders!” подчеркивает обращение к целевой аудитории',
   },
   {
     id: '2',
-    link: 'https://owlcat.games/news/78',
-    title: 'OwlCat',
-    image: 'https://i.ibb.co/6yKj2vq/pathfinder.jpg',
-    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    link: 'https://mundfish.com/',
+    title: 'Mundfish',
+    image: 'https://i.ibb.co/Tb5XwMm/Atomic-Heart.jpg',
+    about: 'Новость о старте предзаказа для рекламы продукта',
     explanation:
-      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+      'Главная новость студии размещена на главной странице сайта. Весь акцент направлен на нее',
   },
   {
     id: '3',
-    link: 'https://owlcat.games/news/78',
-    title: 'OwlCat',
-    image: 'https://i.ibb.co/6yKj2vq/pathfinder.jpg',
-    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    link: 'https://www.guerrilla-games.com/read/creating-the-many-faces-of-horizon-forbidden-west',
+    title: 'Guerrilla Games',
+    image: 'https://i.ibb.co/ZdvzCnM/Horizon.jpg',
+    about:
+      'Анонс события с демонстрацией технологических возможностей студии. Привлечение новый специалистов в свою команду',
     explanation:
-      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+      'Тезисно выделено о чем будет рассказано на встрече. Дизайн страницы с использованием изображения крупного плана также подчеркивает суть',
   },
   {
     id: '4',
-    link: 'https://owlcat.games/news/78',
-    title: 'OwlCat',
-    image: 'https://i.ibb.co/6yKj2vq/pathfinder.jpg',
-    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    link: 'https://blog.quanticdream.com/ru/netease-games-%d0%b8-quantic-dream-%d1%81%d1%82%d1%80%d0%be%d0%b8%d0%bc-%d0%bd%d0%b0%d1%88%d0%b5-%d0%b1%d1%83%d0%b4%d1%83%d1%89%d0%b5%d0%b5-%d0%b2%d0%bc%d0%b5%d1%81%d1%82%d0%b5/',
+    title: 'Quantic Dream',
+    image: 'https://i.ibb.co/vc9PLwc/Detroit.png',
+    about:
+      'Информирование игроков о присоединении студии к компании NetEase Games',
     explanation:
-      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+      'В статье рассказана краткая предыстория кампании и почему было принято такое решение. Статья не выделяется на фоне остальных новостей, но призвана успокоить свою аудиторию',
   },
   {
     id: '5',
-    link: 'https://owlcat.games/news/78',
-    title: 'OwlCat',
-    image: 'https://i.ibb.co/6yKj2vq/pathfinder.jpg',
-    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    link: 'https://news.blizzard.com/ru-ru/diablo4/23880208/vstrecha-s-tvorcom-sostoitsya-6-iyunya-oformite-predzakaz-diablo-iv?blzcmp=blizzard-news',
+    title: 'Blizzard Entertainment ',
+    image: 'https://i.ibb.co/F344xHC/Diablo-4.jpg',
+    about: 'Новость о старте предзаказа для рекламы продукта',
     explanation:
-      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+      'Новость рассказывает о возможных версиях копий и что в них ждет игрока. Новость также размещена на главной странице сайта для привлечения внимания',
   },
 ];
 
@@ -65,4 +68,4 @@ export const analyze = {
   title: 'Patch 1',
   image: 'https://i.ibb.co/6yKj2vq/pathfinder.jpg',
   text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
-}
+};
